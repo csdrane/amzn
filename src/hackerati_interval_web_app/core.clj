4dec8038bd5354e9c573b0b74d91d46e27491a21
Chris Drane <csdrane@gmail.com>
2014-11-03 16:25:53 -0500

    Refactored let bindings out of views.clj and into core.clj. There's still a lot of redundancy which can be solved by rolling bindings up into a macro / perhaps a fn.

--- src/hackerati_interval_web_app/core.clj
+++ src/hackerati_interval_web_app/core.clj
@@ -3,7 +3,6 @@
 ; If lein run is crashing, you are probably forgetting the port number.
 (ns hackerati-interval-web-app.core
   (:require  [compojure.core :refer :all]
-             [compojure.handler :as handler]
              [compojure.route :as route]
              [hackerati-interval-web-app.views.csv :as csv]
              [hackerati-interval-web-app.views.views :as views]
@@ -13,19 +12,42 @@
              [ring.middleware.defaults :refer :all]
              [ring.middleware.session.cookie])
   (:use [ring.middleware.json :only [wrap-json-response]])
-  (:import (java.util.concurrent ScheduledThreadPoolExecutor TimeUnit)) 
+  (:import (java.util.concurrent ScheduledThreadPoolExecutor TimeUnit))
   (:gen-class))
 
+(defroutes POST-routes
+  (POST "/login" {{username :username} :params
+                  {password :password} :params}
+    ((fn [v] (assoc (:response-map v)
+               :session {:username (:username v)}))
+     (views/login username password)))
+  (POST "/newlink" {{username :username} :session
+                    {link :link} :params
+                    {description :description} :params}
+    (views/add-link! username link description))
+  (POST "/register" {{username :username} :params
+                     {password :password} :params
+                     {email :email} :params}
+    (views/attempt-register username password email))
+  (POST "/delete-link" {{username :username} :session
+                        {actionid :actionid} :params}
+    (views/delete-link! username actionid))
+  (POST "/editlink" {{username :username} :session
+                     {actionid :pk} :params
+                     {description :value} :params}
+    (views/edit-link! username actionid description)))
+
+(defroutes GET-routes
+  (GET "/link/:productid" {{username :username} :session
+                           {productid :productid} :params}
+    (views/link-view username productid))
+  (GET "/csv/:productid" {{productid :productid} :params} (csv/chart-csv productid))
+  (GET "/" {{:keys [username]} :session} (views/index username))
+  (GET "/login" {{:keys [message]} :request} (views/logged-out message)))
+
 (defroutes main-routes
-  (GET "/" request (views/index request))
-  (POST "/delete-link" request (views/delete-link! request))
-  (POST "/editlink" request (views/edit-link! request))
-  (GET "/link/:productid" request (views/link-view request))
-  (GET "/login" {session :session}  (views/logged-out))
-  (POST "/login" request (views/login request))
-  (POST "/newlink" request (views/add-link! request))
-  (POST "/register" request (views/attempt-register request))
-  (GET "/csv/:productid" [productid] (csv/chart-csv productid))
+  GET-routes
+  POST-routes
   (route/resources "/bootstrap")
   (route/not-found "<h1>Page not found</h1>"))
 
@@ -34,8 +56,8 @@
 (def app
   (wrap-json-response
    (wrap-defaults
-    (handler/site main-routes)
-    (-> (assoc-in site-defaults [:security :anti-forgery] false)  
+    main-routes
+    (-> (assoc-in site-defaults [:security :anti-forgery] false)
         (assoc-in [:store] (ring.middleware.session.cookie/cookie-store {:key "a 16-byte secret"}))))
    :pretty))
 
@@ -45,14 +67,13 @@
 
 ;; Scheduled task must go first, otherwise won't be executed.
 (defn -main [& [port]]
-  (if-let [port (try 
-                  (Integer/parseInt port) 
-                  (catch Exception e))] 
+  (if-let [port (try
+                  (Integer/parseInt port)
+                  (catch Exception e))]
     (do
       (System/setProperty "java.awt.headless" "true")
       (scheduled-task db/refresh-prices)
       (stay-alive 1)
-      (jetty/run-jetty app 
+      (jetty/run-jetty app
                        {:port port :auto-reload? true}))
     (println "Useage: lein run port")))
-
--- src/hackerati_interval_web_app/views/views.clj
+++ src/hackerati_interval_web_app/views/views.clj
@@ -1,5 +1,5 @@
 (ns hackerati-interval-web-app.views.views
-  (:require [clojure.data.json :as json] 
+  (:require [clojure.data.json :as json]
             [hackerati-interval-web-app.views.template :as template]
             [hackerati-interval-web-app.views.util :as util]
             [hackerati-interval-web-app.schema :as db]
@@ -28,75 +28,57 @@
     (if-let [username (-> request :params :username)]
       username)))
 
-(defn add-link! [request] 
-  (let [{session :session} request
-        {params :params} request
-        {link :link} params
-        {description :description} params
-        {username :username} session]
-    (if (util/valid-link? link)
-      (try 
-        (assoc (response {:success true 
-                          :actionid (:generated_key 
-                                     (db/add-link! 
-                                       (db/get-user-id username) 
-                                         link (escape-html description)))})
-          :headers {"Content-Type" "text/javascript;charset=UTF-8"})
-        (catch Exception e (str "Add link failed! " e)))
-      (assoc (response {:success false, :msg "<strong>Error: Not a valid link!</strong>"})
-        :status 500))))
+(defn add-link! [username link description]
+  (if (util/valid-link? link)
+    (try
+      (assoc (response {:success true
+                        :actionid (:generated_key
+                                   (db/add-link!
+                                    (db/get-user-id username)
+                                    link (escape-html description)))})
+        :headers {"Content-Type" "text/javascript;charset=UTF-8"})
+      (catch Exception e (str "Add link failed! " e)))
+    (assoc (response {:success false, :msg "<strong>Error: Not a valid link!</strong>"})
+      :status 500)))
 
 ;; TODO add message indicating successful operation; currently returns 404
-(defn delete-link! [request]
-  (let [{session :session} request
-        {username :username} session
-        {params :params} request
-        {actionid :actionid} params]
-    (if (db/authorized-link? {:username username :actionid actionid})
-      (try  
-        (do (db/delete-link! username actionid)
-            (str "Delete link successful!"))
-        (catch Exception e "Error: deletion failed!")))))
+(defn delete-link! [username actionid]
+  (if (db/authorized-link? {:username username :actionid actionid})
+    (try
+      (do (db/delete-link! username actionid)
+          (str "Delete link successful!"))
+      (catch Exception e "Error: deletion failed!"))))
 
-(defn edit-link! [request]
-  (let [{session :session} request
-        {username :username} session
-        {params :params} request
-        {actionid :pk} params
-        {description :value} params]
-    (if (db/authorized-link? {:username username :actionid actionid})
-      (try
-        (do 
-          (db/edit-link! actionid (escape-html description))
-          (str "{success: true}"))
-        (catch Exception e "Error: deletion failed")))))
+(defn edit-link! [username actionid description]
+  (if (db/authorized-link? {:username username :actionid actionid})
+    (try
+      (do
+        (db/edit-link! actionid (escape-html description))
+        (str "{success: true}"))
+      (catch Exception e "Error: deletion failed"))))
 
-(defn link-view 
+(defn link-view
   "Display prices after clicking on tracked link"
-  [request]
-  (let [{session :session} request
-        {username :username} session
-        {params :params} request
-        {productid :productid} params]
-    (template/site-template
-     (if (db/authorized-link? {:username username :productid productid}) 
-       (html 
-        [:h3 "Replace with :description"]
-        [:div {:id "graphdiv" :productid productid}]
-        [:div
-         [:table {:class "table table-striped table-condensed"}
-          [:thead
-           [:tr [:th "Date"] [:th "Price"]]
-           [:tbody
-            (for [{date :date price :price} (db/get-prices productid)]
-              [:tr [:td date] [:td price]])]]]] 
-        [:a {:href "/" :class "small"} "back"] 
-        (include-js "/dygraph-combined.js") 
-        (include-js "/amzn-chart.js"))
-       (html [:h1 "Not authorized for access!"])))))
+  [username productid]
+  (template/site-template
+   (if (db/authorized-link? {:username username :productid productid})
+     (html
+      [:h3 "Replace with :description"]
+      [:div {:id "graphdiv" :productid productid}]
+      [:div
+       [:table {:class "table table-striped table-condensed"}
+        [:thead
+         [:tr [:th "Date"] [:th "Price"]]
+         [:tbody
+          (for [{date :date price :price} (db/get-prices productid)]
+            [:tr [:td date] [:td price]])]]]]
+      [:a {:href "/" :class "small"} "back"]
+      (include-js "/dygraph-combined.js")
+      (include-js "/amzn-chart.js"))
+     (html [:h1 "Not authorized for access!"]))))
 
-(defn logged-in? [session]
-  (contains? session :username))
+(defn logged-in? [username]
+  (some username))
 
 (defn- tracked-links-html [username]
   (html
@@ -104,72 +86,63 @@
     [:table {:class "table table-striped table-condensed" :id "links-table"}
      [:thead [:tr [:th "Link to Amazon"] [:th "Description"] [:th {:class "delete-button"}]]]
      [:tbody
-      (for [{url :url 
+      (for [{url :url
              description :description
              productid :productid
-             actionid :actionid} 
+             actionid :actionid}
             (db/get-links username)]
         [:tr {:id actionid}
          [:td [:a {:href url} url]]
          [:td [:a {:href (str "link/" productid) :class "description-editable"
                    :data-url "/editlink" :data-pk actionid} description]]
-         [:td {:class "delete-button"} 
-           [:button {:type "button" :class "close" 
+         [:td {:class "delete-button"}
+           [:button {:type "button" :class "close"
                      :onclick "deleteRow(this)"} "&times;"]]])]]]))
 
-(defn logged-in [request]
-  (let [username (get-username-from-request-map request)
-        {session :session} request]
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body (template/site-template (html 
-                           [:h3 "Links you're following, " (str username)] [:br] [:br]
-                           (tracked-links-html username)
-                           [:div {:class "small" :id "editing-fields"} 
-                             [:a {:href "#" :id "add-links"} "add"] 
-                             " " 
-                             [:a {:href "#" :id "edit-links"} "edit"]] 
-                           [:br]
-                           [:div {:id "msg" :class "alert hide"}]
-                           (debug request)))
-     :session (assoc session :username username)}))
+(defn logged-in [username]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (template/site-template (html
+                                  [:h3 "Links you're following, " (str username)] [:br] [:br]
+                                  (tracked-links-html username)
+                                  [:div {:class "small" :id "editing-fields"}
+                                   [:a {:href "#" :id "add-links"} "add"]
+                                   " "
+                                   [:a {:href "#" :id "edit-links"} "edit"]]
+                                  [:br]
+                                  [:div {:id "msg" :class "alert hide"}]))})
 
-(defn logged-out [& {:keys [message] :or {message ""}}] 
+(defn logged-out [message]
   "View of site when not logged in. Accepts optional request parameter for use when debugging is enabled."
   (template/site-template
    (html [:div {:class "logged-out-container"}
           (form-to [:post "/login"]
-                   (template/input-field-with-label "username") 
+                   (template/input-field-with-label "username")
                    (template/password-field-with-label "password")
+                   (anti-forgery-field)
                    (submit-button "log in"))]
     [:br] [:br]
-    [:div {:class "logged-out-container"} 
+    [:div {:class "logged-out-container"}
      (form-to [:post "/register"]
               (template/input-field-with-label "username")
               (template/password-field-with-label "password")
               (template/input-field-with-label "email")
               (anti-forgery-field)
               (submit-button "submit"))]
-    [:br] [:br] 
-    (if (seq message) 
+    [:br] [:br]
+    (if (seq message)
       [:div {:class "alert alert-info"} message]))))
 
-(defn index [request]
-  (let [{session :session} request] 
-    (if (logged-in? session)
-      (if (db/exists? :username (session :username))
-        (logged-in request))
-      (logged-out))))
+(defn index [username]
+  (if (and username (db/exists? :username username))
+    (logged-in username)
+    (logged-out nil)))
 
-(defn login [request]
-  (let [{params :params} request
-        {username :username} params  
-        {password :password} params
-        {session :session} request
-        message "Error: username/password invalid!"]
+(defn login [username password]
+  (let [message "Error: username/password invalid!"]
     (if (db/valid-user? username password)
-      (logged-in request)
-      (logged-out :message message))))
+      {:response-map (logged-in username) :username username}
+      (logged-out (str message "username: " username "password: " password)))))
 
 (defn email-exists []
   (logged-out :message "Sorry, email already exists!"))
@@ -189,15 +162,12 @@
 (defn user-exists []
   (logged-out :message "Sorry, user exists!"))
 
-(defn attempt-register [{params :params}]
-  (let [username (:username params)
-        pw (:password params)
-        email (:email params)]
-    (cond 
-     ((comp not util/valid-username?) username) (invalid-username)
-     ((comp not util/valid-email?) email) (invalid-email)
-     (db/exists? :username  username) (user-exists)
-     (db/exists? :email email) (email-exists)
-     :else (do
-             (db/add-user! username pw email)
-             (registration-successful)))))
+(defn attempt-register [username pw email]
+  (cond
+   ((comp not util/valid-username?) username) (invalid-username)
+   ((comp not util/valid-email?) email) (invalid-email)
+   (db/exists? :username username) (user-exists)
+   (db/exists? :email email) (email-exists)
+   :else (do
+           (db/add-user! username pw email)
+           (registration-successful))))
